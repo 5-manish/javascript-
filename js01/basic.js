24e1c1536546e6c4a02ab2397ea70f45666f5950
5-manish <baghelmanish0506@gmail.com>
2026-05-03 10:43:02 +0000

    container config done

--- js01/basic.js
+++ js01/basic.js
@@ -1,1 +1,15 @@
 console.log("Hello, JavaScript!");
+
+const id=101
+let name="Amma"
+var age=45
+city="abd"
+
+console.table([id,name,age,city])
+
+// id=102
+name="Dada"
+age=52
+city="lucknow"
+
+console.table([id,name,age,city])
