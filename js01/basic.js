834bb6f9b30296075ce3cdf969ad9316cf444d2e
5-manish <baghelmanish0506@gmail.com>
2026-05-04 12:12:59 +0000

    operators

--- js01/basic.js
+++ js01/basic.js
@@ -5,11 +5,27 @@ let name="Amma"
 var age=45
 city="abd"
 
-console.table([id,name,age,city])
+//console.table([id,name,age,city])
 
 // id=102
 name="Dada"
 age=52
 city="lucknow"
 
-console.table([id,name,age,city])
+//console.table([id,name,age,city])
+
+// alert(3+3) not work here this is run in browser console
+
+// let x=4
+// y=x++
+// console.log(x,y)
+
+// z=++x
+// console.log(z)
+
+// a=--z
+// b=z--
+// console.log(a,b,z)
+
+console.log("2"==2) //only value check
+console.log("2"===2) // value and type check
