f1790a9e1df9b2ba178f91d160e76ef9e321c486
5-manish <baghelmanish0506@gmail.com>
2026-05-08 04:25:24 +0000

    Array basic

--- js01/array.js
+++ js01/array.js
@@ -1,2 +1,40 @@
-const arr=[1,2,3,4,5]
-console.log(arr[1])
+// const arr=[1,2,3,4,5]
+// console.log(arr[1])
+
+
+// //Slice and Splice in Array
+
+// //const myarr=[0,1,2,3,4,5]
+// const OriginalArr=new Array("A",0,1,2,3,4,5)
+// console.log("Original array  ", OriginalArr);
+
+
+// //slice
+// const arr1 = OriginalArr.slice(1,3)
+// console.log("After slicing ", arr1);
+// console.log("Original array A ", OriginalArr);
+
+
+// //splice
+// const arr2 = OriginalArr.splice(1,3)
+// console.log("Array after splicing ", arr2)
+// console.log("Original array  B", OriginalArr);
+
+// Array concatenation
+ //wrong method
+
+ const arr1=[1,2,3]
+ const arr2=[4,5,6]
+
+//  const arr3=arr1.push(arr2)
+//  console.log(arr3)
+
+// const arr3=arr1.concat(arr2)
+// console.log(arr3)
+
+//Spread method
+// const arr3 = [...arr1, ...arr2]
+// console.log(arr3)
+
+// const mixArr=[1,2,[3,4],[5,[6,7,8]],9]
+// console.log(mixArr.flat(Infinity))
---
+++ js01/basic_maths.js
@@ -0,0 +1,31 @@
+const no = 10
+
+//console.log(typeof no);
+
+const balance = new Number(100)
+//console.log(balance);
+
+//console.log(balance.toFixed(3));  //how many digits after decimal
+
+//console.log(balance.toPrecision( )) //ek baar chatgpt kar lena
+
+/// Date
+
+let myDate = new Date()
+// console.log(myDate)
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toTimeString())
+// console.log(myDate.toISOString())
+// console.log(myDate.toLocaleDateString())
+
+//Apni marzi ki date rakhna ho 
+// const date1=new Date(2023,4,8,5,44,50)
+//  console.log(date1.toDateString())
+// console.log(date1)
+// console.log(date1.getMonth())
+
+
+
+
+
